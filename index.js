fb3213a0d9b430252f3a80b1b8f9cf6a2d79907c
Sarojyarsian <saroj.shrestha@yarshastudio.com>
2021-08-09 11:44:05 +0545

    v1.1 : concatenating string using back ticks

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-/**creating variable (value can be changed overtime)**/
+/* v0.1 CREATING VARAIBLE WITH VAR (value can be changed overtime) */
 // var var_name = "Ryan";
 // var_name = 'Zen'; //overrides to previous same variable
 // alert(name);
 
-/**creating constant (whose  value cannot be changed)**/
+/* v0.1 CREATING VARIABLE WITH CONST (whose  value cannot be changed) */
 // const const_name = 'Ryan';
 // it will show error since u can't override const variable 
 // const_name = 'Zen'; 
 // alert(const_name);
 
-//creating variables using let
+/* v0.2 CREATING VARIABLE WITH LET */
 // if (true) {
 //     let let_name = 'Ryan';
 //     let_name = 'Zen'; //let variable can be override
 //     // alert(let_name); //alerts let_name
 // }
 // alert(let_name); //doesn't alert (*Uncaught ReferenceError: let_name is not defined)
+
+/* v1.1 TEMPLATE STRING (added in ES) */
+let fname = 'Saroj';
+let lname = 'Shrestha';
+let age = prompt("Guess Ryan's age... ");
+
+//old way
+// let result = fname + ' ' + lname + ' is ' + age + ' years old.';
+// alert(result)
+
+//using tempalte string;
+
+let result = `${fname} ${lname} is ${age} years old.`;
+alert(result);
