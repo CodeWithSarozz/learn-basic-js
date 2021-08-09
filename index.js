d5e5ac4be2d2a1fdb9946bcebd3dd2a328ce3d32
Sarojyarsian <saroj.shrestha@yarshastudio.com>
2021-08-09 11:35:24 +0545

    push 0.2 : creating variable with let

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-//creating variable (value can be changed overtime)
-var name = "Ryan";
-name = 'Zen'; //overrides to previous same variable
+/**creating variable (value can be changed overtime)**/
+// var var_name = "Ryan";
+// var_name = 'Zen'; //overrides to previous same variable
 // alert(name);
 
-//creating constant (whose  value cannot be changed)
-const name2 = 'Ryan';
-name2 = 'Zen'; /** it will show error since u can't override const variable  */
-alert(name2);
+/**creating constant (whose  value cannot be changed)**/
+// const const_name = 'Ryan';
+// it will show error since u can't override const variable 
+// const_name = 'Zen'; 
+// alert(const_name);
+
+//creating variables using let
+// if (true) {
+//     let let_name = 'Ryan';
+//     let_name = 'Zen'; //let variable can be override
+//     // alert(let_name); //alerts let_name
+// }
+// alert(let_name); //doesn't alert (*Uncaught ReferenceError: let_name is not defined)
