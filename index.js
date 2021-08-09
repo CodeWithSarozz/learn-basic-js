82694b8da0f47d0773a9a91210c8495be9d09f26
Sarojyarsian <saroj.shrestha@yarshastudio.com>
2021-08-09 11:26:36 +0545

    v0.1 : learning about JS variables

---
+++ index.js
@@ -0,0 +1,9 @@
+//creating variable (value can be changed overtime)
+var name = "Ryan";
+name = 'Zen'; //overrides to previous same variable
+// alert(name);
+
+//creating constant (whose  value cannot be changed)
+const name2 = 'Ryan';
+name2 = 'Zen'; /** it will show error since u can't override const variable  */
+alert(name2);
